eabff50a81c47b404a2f5ffb8a89277c14a3dda0
Alexandros5880 <alexandrosplatanios15@gmail.com>
2023-11-20 20:51:05 +0200

    Add Encoder Decoeder functionallity

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Example:
-`python html_gzip_encoder.py HTML_FILES/hotspot_main.html GZ_FILES/hotspot_main.html.gz`
+`python html_gzip_encoder.py e HTML_FILES/hotspot_main.html OUTPUT/hotspot_main.html.gz`
+`python html_gzip_encoder.py d GZ_FILES/hotspot_main.html.gz OUTPUT/hotspot_main.html`
+
+### Options:
+`e`: **Encode html to gzip**
+`d`: **Decode gzip to html**
--- html_gzip_encoder.py
+++ html_gzip_encoder.py
@@ -9,8 +9,8 @@ def create_gzip_file(filepath, gzipfile):
     with gzip.open(gzipfile, 'wb') as gzip_file:
       gzip_file.write(str.encode('utf-8'))
 
-def print_decode_gzip_file_to_hex(filepath):
-  with gzip.open(filepath, mode='r') as infile:
+def print_decode_gzip_file_to_hex(gzip_file):
+  with gzip.open(gzip_file, mode='r') as infile:
     bytes_result = infile.read()
     text = bytes_result.decode('utf-8')
     hex_array = []
@@ -28,18 +28,24 @@ def print_decode_gzip_file_to_hex(filepath):
       else:
         print(c, end=", ")
         
-def print_decode_gzip_file_to_string(filepath):
-  with gzip.open(filepath, mode='r') as infile:
+def print_decode_gzip_file_to_string(gzipfile, filepath):
+  with gzip.open(gzipfile, mode='r') as infile:
     bytes_result = infile.read()
     text = bytes_result.decode('utf-8')
     print("Len: ", len(text))
     print("\nText:\n")
-    line_size = 17
-    index = 0
     print(text)
+    with open(filepath, mode='w') as f_out:
+      f_out.write(text)
     
 if __name__=='__main__':
-  htmlfile = sys.argv[1]
-  gzipfile = sys.argv[2]
-  create_gzip_file(htmlfile, gzipfile)
-  print_decode_gzip_file_to_hex(gzipfile)
+  option = sys.argv[1]
+  if option == 'e':
+    text_file = sys.argv[2]
+    gzip_file = sys.argv[3]
+    create_gzip_file(text_file, gzip_file)
+    print_decode_gzip_file_to_hex(gzip_file)
+  elif option == 'd':
+    gzip_file = sys.argv[2]
+    text_file = sys.argv[3]
+    print_decode_gzip_file_to_string(gzip_file, text_file)
